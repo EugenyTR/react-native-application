36ae3b5d52ee78fb049e0dc84b67ccaa9b3550c4
Eugeny Semenov <et@takeseoeasy.com>
2020-09-04 23:17:15 +0700

    add FlatList, TextInput, styled all this. Add key

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
-import React, {
-  useState
-} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button
-} from 'react-native';
+import React, { useState } from 'react';
+import {StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList} from 'react-native';
 
 export default function App() {
-  return ( 
-    <View style={styles.headingPadding}>
-      <View>
-        <TextInput style={styles.textInputPadding} placeholder="Course Goal" />
-        <Button title="ADD" />
-      </View>
-      <View>
-      
-      </View>
-    </View>
-  );
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    };
+
+    const addGoalHandler = () => {
+        setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: enteredGoal }]);
+    };
+    return (
+        <View style={styles.headingPadding}>
+          <View style={styles.flexibleRow}>
+            <TextInput
+                style={styles.textInputPadding}
+                placeholder="Course Goal"
+                onChangeText={goalInputHandler}
+                value={enteredGoal}
+            />
+            <Button title="ADD" onPress={addGoalHandler} />
+          </View>
+          <FlatList
+              keyExtractor={(item, index) => item.id}
+              data={courseGoals}
+              renderItem={itemData => (
+              <View style={styles.listItem}>
+                  <Text>{itemData.item.value}</Text>
+              </View>
+            )}
+          />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -31,5 +44,21 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomColor: '#000',
     borderBottomWidth: 1,
+      width: '80%',
+      marginBottom: '5%',
   },
+    flexibleRow: {
+      flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    goal: {
+        width: '100%',
+    },
+    listItem: {
+        padding: 10,
+        backgroundColor: '#eee',
+        borderColor: '#fff',
+        borderWidth: 1,
+    }
 });
